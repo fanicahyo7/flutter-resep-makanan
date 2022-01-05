a432806393343e9ffb1653665b75fbc0f56f4a13
fanidc7 <fanidc7@gmail.com>
2022-01-05 15:41:23 +0700

    first init & header home page

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_resep_makanan/models/resep.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    List<Resep> mResep = masterResep;
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+              margin: const EdgeInsets.only(top: 50, left: 24),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Resep Makanan',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  const Text(
+                    'Temukan Resep Makanan Terbaikmu Disini',
+                    style: TextStyle(fontSize: 14),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: mResep.map((e) => (Text(e.judul))).toList(),
+                  ),
+                ],
+              )),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/resep_card.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+
+class ResepCard extends StatelessWidget {
+  const ResepCard({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
